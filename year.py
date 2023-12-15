cca6c7bdd598a88240caa36da2b388a516e3e971
Swathi Indla <119476796+SwathiIndla@users.noreply.github.com>
2023-12-15 10:54:07 +0530

    Update year.py

--- year.py
+++ year.py
@@ -1,12 +1,15 @@
 import sys
+
 def is_leap_year(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     else:
         return False
-year_to_check =int(sys.argv[1])
-result = is_leap_year(year_to_check)
-if result:
-    print(f"{year_to_check} is a leap year.")
-else:
-    print(f"{year_to_check} is not a leap year.")
+
+if __name__ == "__main__":
+    year_to_check = int(sys.argv[1])
+    result = is_leap_year(year_to_check)
+    if result:
+        print(f"{year_to_check} is a leap year.")
+    else:
+        print(f"{year_to_check} is not a leap year.")
